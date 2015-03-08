218c70791b19023d1165610661ec49f35a3db857
aikeay <aikeay@fullsail.edu>
2015-03-07 18:15:15 -0800

    Finished Goal1 assignment

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -24,7 +24,7 @@ Assignment: Week#1: ANALYZE Duel#1
     var playerOneHealth= 100;
     var playerTwoHealth= 100;
 
-    //round
+   //round
     var round= 0;
 
     function fight(){
@@ -32,16 +32,43 @@ Assignment: Week#1: ANALYZE Duel#1
 
         alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth)
 
+        for(var i=0; i < 10; i++){
 
+            //random formula is - Math.floor(Math.random()*(max-min)+min);
+
+            var minDamage1 = player1Damage *.5;
+            var minDamage2 = player2Damage *.5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+
+
+            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if(results === "no winner"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" "+playerTwoName+":"+playerTwoHealth);
+            }else{
+                alert(results);
+                break;
+            };
+        };
     };
 
     function winnerCheck(){
-        console.log("in winnerCheck FN");
+       console.log("in winnerCheck FN");
 
         var result="no winner";
 
         if(playerOneHealth<1 && playerTwoHealth<1){
-            result = "You Both Die";
+            result = "No winner!";
         }else if(playerOneHealth<1){
             result =playerTwoName+"WINS!!!"
         }else if(playerTwoHealth<1) {
