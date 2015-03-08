8077173c18d59e98722d470c47998c715e047b7e
aikeay <aikeay@fullsail.edu>
2015-03-07 18:08:02 -0800

    created player var

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -7,3 +7,21 @@ Date: March 07 2015
 Assignment: Week#1: ANALYZE Duel#1
 */
 
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName= "Superman";
+    var playerTwoName= "Flash";
+
+    //player damage
+    var player1Damage= 20;
+    var player2Damage= 20;
+
+    //player health
+    var playerOneHealth= 100;
+    var playerTwoHealth= 100;
+
+   })();
