df9bce19615c1a026a29580b6fafb379862504d9
aikeay <aikeay@fullsail.edu>
2015-03-12 22:48:09 -0700

    switched out information
    switched out previous information with array information

--- Wk2/goal2_assign_duel2/goal2_assign_duel2/js/main.js
+++ Wk2/goal2_assign_duel2/goal2_assign_duel2/js/main.js
@@ -20,7 +20,7 @@ Assignment: Week#1: ANALYZE Duel#1
     function fight(){                           //create function to run fight
         console.log("in the fight function")    //console.log to report that the fight in the function
                                                 //report to the user the players health at the start of the fight
-        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth)
+        alert(player1[0]+":"+player1[2]+" *START* "+player2[0]+":"+player1[2]);
 
         for(var i=0; i < 10; i++){              //for loop to run the fight
 
@@ -31,12 +31,12 @@ Assignment: Week#1: ANALYZE Duel#1
             var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
             var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
 
-            playerOneHealth-=f1;                //inflict damage
-            playerTwoHealth-=f2;                //inflict damage
+            player1[2]-=f1;                    //inflict damage
+            player2[2]-=f2;                    //inflict damage
 
 
                                                 //console.log the players health after each round
-            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
+            console.log(player1[0]+":"+player1[2]+" "+player2[0]+":"+player2[2]);
 
             var results = winnerCheck();        //create var for the results of the match
             console.log(results);               //console.log the results
@@ -44,7 +44,7 @@ Assignment: Week#1: ANALYZE Duel#1
             if(results === "no winner"){        //if else function for fight
                 round++;                        //round numbers
                                                 //report to the user the round number and players health
-                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" "+playerTwoName+":"+playerTwoHealth);
+                alert(player1[0]+":"+player1[2]+" *ROUND "+round+" "+player2[0]+":"+player2[2]);
             }else{
                 alert(results);                 //report the results to user
                 break;                          //end of function
@@ -57,15 +57,15 @@ Assignment: Week#1: ANALYZE Duel#1
 
         var result="no winner";                 //create var results for no winner
                                                 //if else function to report fight winner
-        if(playerOneHealth<1 && playerTwoHealth<1){
+        if(player1[2]<1 && player2[2]<1){
             result = "No winner!";
-        }else if(playerOneHealth<1){
-            result =playerTwoName+"WINS!!!"
-        }else if(playerTwoHealth<1) {
-            result = playerOneName + "WINS!!!"
+        }else if(player1[2]<1){
+            result =player2[0]+"WINS!!!"
+        }else if(player2[2]<1) {
+            result = player1[0] + "WINS!!!"
         };
 
-        return result;                          //return results to the outside of the funtion
+        return result;                          //return results to the outside of the function
 
     };
 
