d5cf97241e0f99fca9c57d2325dd049c7507df5c
aikeay <aikeay@fullsail.edu>
2015-03-20 13:01:41 -0700

    changed all the names in assignment to match object names

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -3,8 +3,8 @@
  */
 /*
 Name: Alana Keay
-Date: March 13, 2015
-Assignment: Week#2: ANALYZE Duel#2
+Date: March 20, 2015
+Assignment: Week#3: ANALYZE Duel#3
 */
 
 //self-executing function
@@ -12,31 +12,30 @@ Assignment: Week#2: ANALYZE Duel#2
 
     console.log("FIGHT!!!");                    //console.log to report the start of the fight
 
-    var player1 = ["Superman", 20, 100];        //create array to contain information for player 1
-    var player2 = ["Flash", 20, 100];           //create array to contain information for player 2
+    player1 = {name:"Superman", damage:20, health:100};        //create array to contain information for player 1
+    player2 = {name:"Flash", damage:20, health:100};           //create array to contain information for player 2
 
     var round= 0;                               //create var round
 
     function fight(){                           //create function to run fight
-        console.log("in the fight function")    //console.log to report that the fight in the function
                                                 //report to the user the players health at the start of the fight
-        alert(player1[0]+":"+player1[2]+" *START* "+player2[0]+":"+player1[2]);
+        alert(player1.name+":"+player1.health+" *START* "+player2.name+":"+player1.health);
 
         for(var i=0; i < 10; i++){              //for loop to run the fight
 
                                                 //random formula is - Math.floor(Math.random()*(max-min)+min);
 
-            var minDamage1 = player1[1] *.5;    //created var for min damage for player 1
-            var minDamage2 = player2[1] *.5;    //created var for min damage for player 2
-            var f1 = Math.floor(Math.random()*(player1[1]-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2[1]-minDamage2)+minDamage2);
+            var minDamage1 = player1.damage *.5;    //created var for min damage for player 1
+            var minDamage2 = player2.damage *.5;    //created var for min damage for player 2
+            var f1 = Math.floor(Math.random()*(player1.health-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2.health-minDamage2)+minDamage2);
 
             player1[2]-=f1;                    //inflict damage
             player2[2]-=f2;                    //inflict damage
 
 
                                                 //console.log the players health after each round
-            console.log(player1[0]+":"+player1[2]+" "+player2[0]+":"+player2[2]);
+            console.log(player1.name+":"+player1.health+" "+player2.name+":"+player2.health);
 
             var results = winnerCheck();        //create var for the results of the match
             console.log(results);               //console.log the results
@@ -44,7 +43,7 @@ Assignment: Week#2: ANALYZE Duel#2
             if(results === "no winner"){        //if else function for fight
                 round++;                        //round numbers
                                                 //report to the user the round number and players health
-                alert(player1[0]+":"+player1[2]+" *ROUND "+round+" "+player2[0]+":"+player2[2]);
+                alert(player1.name+":"+player1.health+" *ROUND "+round+" "+player2.name+":"+player2.health);
             }else{
                 alert(results);                 //report the results to user
                 break;                          //end of function
@@ -57,12 +56,12 @@ Assignment: Week#2: ANALYZE Duel#2
 
         var result="no winner";                 //create var results for no winner
                                                 //if else function to report fight winner
-        if(player1[2]<1 && player2[2]<1){
+        if(player1.health<1 && player2.health<1){
             result = "No winner!";
-        }else if(player1[2]<1){
-            result =player2[0] + "WINS!!!"
-        }else if(player2[2]<1) {
-            result = player1[0] + "WINS!!!"
+        }else if(player1.health<1){
+            result =player2.name + "WINS!!!"
+        }else if(player2.health<1) {
+            result = player1.name + "WINS!!!"
         };
 
         return result;                          //return results to the outside of the function
